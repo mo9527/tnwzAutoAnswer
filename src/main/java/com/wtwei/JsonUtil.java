06f02b2a3c23b98ebd08a2d7027372c182df653b
weiwt <wtwei@mo9.com>
2018-01-19 12:45:48 +0800

    aa

--- src/main/java/com/wtwei/JsonUtil.java
+++ src/main/java/com/wtwei/JsonUtil.java
@@ -4,12 +4,10 @@ import net.sf.json.JSONObject;
 import net.sf.json.JsonConfig;
 import org.apache.struts2.json.JSONUtil;
 
-import java.util.Date;
 
 public class JsonUtil {
     public static String serialize(Object jsonObj) {
         JsonConfig jsonConfig = new JsonConfig();
-        jsonConfig.registerJsonValueProcessor(Date.class,new JsonDateValueProcessor());
 
         JSONObject jo = JSONObject.fromObject(jsonObj, jsonConfig);
 
